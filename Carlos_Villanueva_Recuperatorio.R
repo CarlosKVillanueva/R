55933d3d14da0de3e7ab524a7e7eaecfbcde884d
Carlos K Villanueva <carloskvillanueva@gmail.com>
2023-06-22 20:07:58 -0300

    sisiis

--- Carlos_Villanueva_Recuperatorio.R
+++ Carlos_Villanueva_Recuperatorio.R
@@ -1,6 +1,5 @@
-# Paso 1: Cargar el dataframe
-url <- "https://raw.githubusercontent.com/CarlosKVillanueva/R/master/mushrooms.csv"
-df <- read.csv(url, stringsAsFactors = TRUE)
+library(readr)
+mushrooms <- read_csv("C:/Users/carlo/OneDrive/Escritorio/Recuperatorio_R/mushrooms.csv")
 
 # Paso 2: Ignorar columnas con valores iguales
 df <- df[, !sapply(df, function(x) all(x == x[1]))]
@@ -18,13 +17,16 @@ df_testing <- df_subset[testing_rows, ]
 # Paso 5: Conjunto de entrenamiento
 df_training <- df_subset[-testing_rows, ]
 
+# Convertir df_training a data.frame
+df_training <- as.data.frame(df_training)
+
 # Paso 6: Contar predicciones incorrectas
 library(rpart)
 # Verificar el nombre exacto de la columna objetivo en tu dataframe
 target_column <- "type"
 
 model <- rpart(as.formula(paste(target_column, "~ .")), data = df_training)
-predicted <- predict(model, newdata = df_testing, type = "class")
+predicted <- predict(model, newdata = df_testing, type = "type")
 actual <- df[testing_rows, target_column]
 incorrect_predictions <- sum(predicted != actual)
 
